5567e09980907f244fe79cc497469b0ba28cd8fa
David <gelpiorama@gmail.com>
2018-04-24 16:18:49 +0200

    dos factorias y casos test adecuados

--- src/org/formacion/factorymethod/Lavadora.java
+++ src/org/formacion/factorymethod/Lavadora.java
@@ -2,16 +2,16 @@ package org.formacion.factorymethod;
 
 public abstract class Lavadora {
 
-	protected boolean tieneTambor;
-	protected boolean tieneMandos;
-	protected String  tipoCarga;
+	protected boolean tieneTambor = false;
+	protected boolean tieneMandos = false;
+	protected String  tipoCarga = null;
 	
 	public void ponerTambor() {
-		tieneTambor = true;
+		this.tieneTambor = true;
 	}
 	
 	public void ponerMandos() {
-		tieneMandos = true;
+		this.tieneMandos = true;
 	}
 
 }
--- test/org/formacion/factorymethod/TestLavadora.java
+++ test/org/formacion/factorymethod/TestLavadora.java
@@ -9,11 +9,18 @@ public class TestLavadora {
 	@Test
 	public void test_carga_frontal() {
 		
-		Lavadora lavadora = new LavadoraCargaFrontal();
-		lavadora.ponerMandos();
-		lavadora.ponerTambor();
+		// Lavadora lavadora = new LavadoraCargaFrontal();
+		// lavadora.ponerMandos();
+		// lavadora.ponerTambor();
 		
+		FactoriaLavadora factoria = new LavadoraCargaFrontalFactoria();
+		assertNotNull(factoria);
+
+		Lavadora lavadora = factoria.creaLavadora();
+		assertNotNull(lavadora);
+
 		assertEquals("frontal", lavadora.tipoCarga);
+		// TestLavadora esta en el mismo paquete que Lavadora: propiedades protected
 		assertTrue(lavadora.tieneMandos);
 		assertTrue(lavadora.tieneTambor);
 	}
@@ -21,9 +28,15 @@ public class TestLavadora {
 	@Test
 	public void test_carga_superior() {
 		
-		Lavadora lavadora = new LavadoraCargaSuperior();
-		lavadora.ponerMandos();
-		lavadora.ponerTambor();
+		// Lavadora lavadora = new LavadoraCargaSuperior();
+		// lavadora.ponerMandos();
+		// lavadora.ponerTambor();
+
+		FactoriaLavadora factoria = new LavadoraCargaSuperiorFactoria();
+		assertNotNull(factoria);
+
+		Lavadora lavadora = factoria.creaLavadora();
+		assertNotNull(lavadora);
 		
 		assertEquals("superior", lavadora.tipoCarga);
 		assertTrue(lavadora.tieneMandos);
